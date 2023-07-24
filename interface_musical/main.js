c4f9eda319e9eec63e934f1af0edc59fe84af0a8
TeodoroPuttfarcken <teodoroputtfarcken@hotmail.com>
2023-07-24 20:25:32 -0300

    Update

--- interface_musical/main.js
+++ interface_musical/main.js
@@ -1,5 +1,12 @@
  function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+    const element = document.querySelector(idElementoAudio).play();
+
+    if (element != null && element.localName === 'audio'){
+        element.play();
+    }
+    else {
+        console.log("Elemento não encontrado");
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla') 
@@ -11,6 +18,15 @@ for (let i = 0; i < listaDeTeclas.length; i++) {
     tecla.onclick = function () {
         tocaSom(idAudio);
     };
+
+    tecla.onkeydown = function (event) {
+        if (event.code === 'Space' || event.code === 'Enter')
+        tecla.classList.add('ativa')
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
     
 }
 
